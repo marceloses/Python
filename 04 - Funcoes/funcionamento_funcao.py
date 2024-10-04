d23b6d676ef9f7cfc971ac072097992c9ed42160
Marcelo Severo <git@github.com:marceloses/APICatalogo.git>
2024-10-03 22:04:39 -0300

    Atualização Funcções

---
+++ 04 - Funcoes/funcionamento_funcao.py
@@ -0,0 +1,15 @@
+
+
+# Functions (Funções)
+
+def bem_vindo():
+    print('Olá Marcelo tudo bem? ')
+    print('Temos muito produtos exelentes')
+
+bem_vindo()
+
+print('Olá qual o seu nome!')
+print('Ola qual o seu nome!')
+print('Olá qual o seu nome!')
+
+bem_vindo()
