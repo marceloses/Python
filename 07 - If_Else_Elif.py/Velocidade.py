a73018c8ffb7f2a187632a7db59f594e95fa44d8
Marcelo Severo <git@github.com:marceloses/APICatalogo.git>
2024-10-30 20:07:33 -0300

    Arquivos.csv

---
+++ 07 - If_Else_Elif.py/Velocidade.py
@@ -0,0 +1,15 @@
+
+corrida = input("Informe a velocidade ")
+corrida = int(corrida)
+
+if corrida > 150:
+    print("Acima de Velocidade favor reduzir:... ")
+
+elif corrida < 70:
+    print("Favor dirigir acima de 80 Km/h: ")
+
+else:
+    print("Velocidade Autorizada! ")
+
+
+
